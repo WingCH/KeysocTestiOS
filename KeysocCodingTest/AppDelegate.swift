d129ab743b463bf8b78443ea13121139a5015453
WingCH <wingch.apps@gmail.com>
2022-03-01 15:59:16 +0800

    downgrade development target to iOS 12, init search and bookmark page

--- KeysocCodingTest/AppDelegate.swift
+++ KeysocCodingTest/AppDelegate.swift
@@ -9,22 +9,25 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let tabBarController = UITabBarController()
+
+        let searchTabNavigationController = UINavigationController(rootViewController: SearchViewController())
+        let searchTabItem = UITabBarItem(title: "Search", image: UIImage(named: "search"), tag: 0)
+        searchTabNavigationController.tabBarItem = searchTabItem
+
+        let bookmarkTabNavigationController = UINavigationController(rootViewController: BookmarkViewController())
+        let bookmarkTabItem = UITabBarItem(title: "Bookmark", image: UIImage(named: "bookmark"), tag: 0)
+        bookmarkTabNavigationController.tabBarItem = bookmarkTabItem
+
+        tabBarController.viewControllers = [searchTabNavigationController, bookmarkTabNavigationController]
+
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
         return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
